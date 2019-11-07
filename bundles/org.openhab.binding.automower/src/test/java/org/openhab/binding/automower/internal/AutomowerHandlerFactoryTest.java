b444f5a2439154f02d944f71617585c5e444bb32
Martin Hagelin <martin@hagelin.nu>
2019-11-07 09:26:53 +0100

    Experimenting...

---
+++ bundles/org.openhab.binding.automower/src/test/java/org/openhab/binding/automower/internal/AutomowerHandlerFactoryTest.java
@@ -0,0 +1,21 @@
+package org.openhab.binding.automower.internal;
+
+import org.eclipse.smarthome.core.thing.Thing;
+import org.eclipse.smarthome.core.thing.ThingUID;
+import org.eclipse.smarthome.core.thing.binding.builder.ThingBuilder;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AutomowerHandlerFactoryTest {
+
+    @Test
+    public void testLogon() {
+        AutomowerHandlerFactory automowerHandlerFactory = new AutomowerHandlerFactory();
+
+        ThingUID thingUID = new ThingUID(AutomowerBindingConstants.APIBRIDGE_THING_TYPE, "HEJ");
+        Thing thing = ThingBuilder.create(AutomowerBindingConstants.APIBRIDGE_THING_TYPE, thingUID).build();
+        automowerHandlerFactory.createHandler(thing);
+    }
+
+}
